b8ca3b87682cf214f96aef1d5b5b9db1ca690d0b
Genta-arya <mgentaarya@gmail.com>
2024-06-29 13:00:27 +0700

    push

--- Config/Databases/db.js
+++ Config/Databases/db.js
@@ -5,5 +5,12 @@ export const pool = mysql.createPool({
     user: 'afyungs2_perpus',
     password: 'Genta456',
     database: 'afyungs2_perpus',
+    port: 3306, 
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+    connectTimeout: 10000 
+    
+
   });
   
