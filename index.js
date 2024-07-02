650f9193bc0c0f006e70e715beb94366a54646ac
Genta-arya <mgentaarya@gmail.com>
2024-07-02 15:55:01 +0700

    add midleware

--- index.js
+++ index.js
@@ -16,15 +16,19 @@ const httpserver = createServer(app);
 // Rate Limiter Middleware
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // Limit each IP to 100 requests per `window` (15 minutes)
+  max: 1000, // Limit each IP to 100 requests per `window` (15 minutes)
   message: "Too many requests from this IP, please try again later.",
 });
 
+app.use(
+  helmet({
+    crossOriginResourcePolicy: false,
+  })
+);
 // Apply rate limiter to all requests
 app.use(limiter);
 
 // Security Headers Middleware
-app.use(helmet());
 
 app.use(express.json());
 app.use(
@@ -34,11 +38,13 @@ app.use(
   })
 );
 
-app.use('/uploads', express.static(path.resolve('uploads')));
+app.use("/uploads", express.static(path.resolve("uploads")));
+
+
 app.use("/api/v1/gallery", routerGallery);
 // Use AuthRouters for authentication routes
 app.use("/api/v1/auth", AuthRouters);
-app.use('/api/v1/storage', router);
+app.use("/api/v1/storage", router);
 
 // Start the server
 httpserver.listen(port, () => {
